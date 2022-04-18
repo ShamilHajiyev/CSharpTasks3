4fe2b19a5cc8905b9ee4c6bc33ef4512ec1a814b
ShamilHajiyev <shamilnh@code.edu.az>
2022-04-19 00:17:32 +0400

    initial commit

---
+++ ArrayMax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many number do you want to compare?");
+            int numCount = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[numCount];
+            int max = 0;
+            ArrayInput(ref numbers, numCount);
+            for (int i = 0; i < numCount; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine("Largest number: " + max);
+        }
+        public static void ArrayInput(ref int[] arr, int itemCount)
+        {
+            
+            arr = new int[itemCount];
+            Console.WriteLine("Enter the numbers:");
+            for (int i = 0; i < itemCount; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
